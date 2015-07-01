3b403c7900dab0d44f7a8d601e8a9bc31e2fa722
abelhabel <anteolsson@gmail.com>
2015-07-01 16:11:04 +0000

    indexing letters added

--- char-count.rb
+++ char-count.rb
@@ -6,5 +6,20 @@ def count_letters(str)
   @hash
 end
 
+def index_letters(str)
+  # str.delete!(' ')
+  @hash = {}
+  @hash.default_proc = proc{ |hash, key| hash[key] = [] }
+  str.split('').each_index {|i| 
+    if str[i] == '' 
+      next
+    else 
+      @hash[str[i]] << i
+    end
+    }
+  @hash
+end
+
 test_string = "aslk jalksdj jaiuwfpninavc  jbcbagsdljahsodiasb asqwhodjkabsdq qn zckjnqkbpihgxcbnmkdf"
 puts count_letters(test_string)
+puts index_letters(test_string)
